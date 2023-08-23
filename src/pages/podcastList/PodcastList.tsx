aeb5da40e9421fa8554de716e550f01acd5d7988
Sergio S <sergi.ss4@gmail.com>
2023-08-24 00:47:17 +0200

    Clean code

--- src/pages/podcastList/PodcastList.tsx
+++ src/pages/podcastList/PodcastList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../store";
